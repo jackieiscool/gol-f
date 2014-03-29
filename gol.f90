acee9ec9eaf914738d39d661ff8cac576ecaeb23
Jackie Herrlin <j.m.herrlin@gmail.com>
2014-03-29 13:37:15 -0700

    Created grid

--- gol.f90
+++ gol.f90
@@ -5,6 +5,7 @@ program gol
   print *, answer
 
   call test_adder       !subroutine call
+  call create_grid
 end program gol
 
 function adder(a, b) result(c)
@@ -20,4 +21,35 @@ subroutine test_adder
   else
     print *, 'test_adder failed'
   endif
+end
+
+subroutine create_grid
+  Integer Grid(10, 10)
+
+  Integer I
+  Integer J
+
+  Do I = 1, 10
+
+    Do J = 1, 10
+
+      If (I .Eq. 5.0) Then
+        Grid(I,J) = 1
+      Else
+        Grid(I,J) = 0
+      End If
+    End Do
+  End Do
+  Write (*,*) Grid
+
+  Do I = 1,10
+    Do J = 1,10
+      If (Grid(I, J).Eq. 1.0) Then
+        If (Grid((I - 1), J) + (Grid((I - 1), (J + 1)) + (Grid((I - 1), (J - 1)) + (Grid((I + 1), (J - 1)) + (Grid((I + 1), J) + (Grid((I + 1), (J + 1)) + (Grid(I, (J - 1)) + (Grid(I, (J + 1))).LE. 1.0 Then
+          Grid(I, J) = 0
+          print *, Grid(I,J)
+        End If
+      End If
+    End Do
+  End Do
 end
